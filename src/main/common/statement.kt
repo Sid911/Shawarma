80a611e7080b63705f60c4f86a9e71026111469e
Janeshsinghania <janeshsinghania2003@gmail.com>
2023-08-09 14:49:48 +0530

    added expression and statement

--- src/main/common/statement.kt
+++ src/main/common/statement.kt
@@ -1,11 +1,11 @@
 
 sealed class Statement{
        class Block(val statements: List<Statement>): Statement()
-       class Expression(val expr: Expr): Statement()
+       class ExpressionSt(val expr: Expression): Statement()
        class Receipe(val name: Token, val params: List<Token>, val body: List<Statement>): Statement()
-       class Taste(val condition: Expr, val thenBranch: Statement, val elseBranch: Statement?): Statement()
-       class Serve(val value: Expr): Statement()
-       class Bill(val keyword: Token, val value: Expr?): Statement()
-       class Ingredient(val name: Token, val initializer: Expr?): Statement()
-       class Stir(val condition: Expr, val body: Statement): Statement()
+       class Taste(val condition: Expression, val thenBranch: Statement, val elseBranch: Statement?): Statement()
+       class Serve(val value: Expression): Statement()
+       class Bill(val keyword: Token, val value: Expression?): Statement()
+       class Ingredient(val name: Token, val initializer: Expression?): Statement()
+       class Stir(val condition: Expression, val body: Statement): Statement()
    }
