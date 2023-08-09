2190667a16b494210d8905bdc9ef01aaf66dd051
Janeshsinghania <janeshsinghania2003@gmail.com>
2023-08-09 14:35:32 +0530

    added expression and statement

--- src/main/common/statement.kt
+++ src/main/common/statement.kt
@@ -1,10 +1,11 @@
-   sealed class Stmt{
-       class Block(val statements: List<Stmt>): Stmt()
-       class Expression(val expr: Expr): Stmt()
-       class Receipe(val name: Token, val params: List<Token>, val body: List<Stmt>): Stmt()
-       class Taste(val condition: Expr, val thenBranch: Stmt, val elseBranch: Stmt?): Stmt()
-       class Serve(val value: Expr): Stmt()
-       class Bill(val keyword: Token, val value: Expr?): Stmt()
-       class Ingredient(val name: Token, val initializer: Expr?): Stmt()
-       class Stir(val condition: Expr, val body: Stmt): Stmt()
+
+sealed class Statement{
+       class Block(val statements: List<Statement>): Statement()
+       class Expression(val expr: Expr): Statement()
+       class Receipe(val name: Token, val params: List<Token>, val body: List<Statement>): Statement()
+       class Taste(val condition: Expr, val thenBranch: Statement, val elseBranch: Statement?): Statement()
+       class Serve(val value: Expr): Statement()
+       class Bill(val keyword: Token, val value: Expr?): Statement()
+       class Ingredient(val name: Token, val initializer: Expr?): Statement()
+       class Stir(val condition: Expr, val body: Statement): Statement()
    }
